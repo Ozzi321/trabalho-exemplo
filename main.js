374ff877a74677562c113bc107647539c4214c66
Luisfelipe <108068028+Ozzi321@users.noreply.github.com>
2024-04-10 12:20:14 -0300

    10042024

---
+++ main.js
@@ -0,0 +1,9 @@
+const dias_falta = document.querySelectorAll(".dias_restantes");
+
+function calcula(){
+    const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
+      const  tempoObjetivo1 = New Date("2024-04-30T23:59:59");
+
+      let tempoAtual = new Date();
+      let tempoFinal =
+}
